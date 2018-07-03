dba96137d2e023d2d775a954b25e9d51e5394168
rydolif <rudolifrudolif@gmail.com>
2018-07-03 17:52:58 +0300

    fontawesome

--- gulpfile.js
+++ gulpfile.js
@@ -46,10 +46,10 @@ gulp.task('js', function() {
 		'app/libs/jquery.mask.min.js',
 		'app/libs/jquery.popupoverlay.js',
 		'app/libs/swiper/swiper.min.js',
-		'app/libs/fancybox/jquery.fancybox.js',
+		// 'app/libs/fancybox/jquery.fancybox.js',
 		'app/libs/animation/wow.min.js',
-		'app/libs/jquery.myParallax.js',
-		'app/libs/video/jquery.mb.YTPlayer.js',
+		// 'app/libs/jquery.myParallax.js',
+		// 'app/libs/video/jquery.mb.YTPlayer.js',
 		'app/js/common.js', // Always at the end
 		])
 	.pipe(plumber())
