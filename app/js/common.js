16d4a7ba221cac7344fb9a4b4d5fb139e98a5adf
rudolif <rudolifrudolif@gmail.com>
2019-01-07 14:05:34 +0200

    scrollpane

--- app/js/common.js
+++ app/js/common.js
@@ -84,24 +84,30 @@ $(function() {
 
   });
 
-  //-------------------------------анімація цифр---------------------------------------
-    var show = true;
-    var countbox = ".about-statistics__container";
-    $(window).on("scroll load resize", function () {
-        if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
-        var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
-        var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
-        var w_height = $(window).height(); // Высота окна браузера
-        var d_height = $(document).height(); // Высота всего документа
-        var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
-        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
-            $('.about-statistics__item h3').spincrement({
-                thousandSeparator: "",
-                duration: 2000
-            });
-            show = false;
-        }
-    });
+  // //-------------------------------анімація цифр---------------------------------------
+  //   var show = true;
+  //   var countbox = ".about-statistics__container";
+  //   $(window).on("scroll load resize", function () {
+  //       if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+  //       var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+  //       var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+  //       var w_height = $(window).height(); // Высота окна браузера
+  //       var d_height = $(document).height(); // Высота всего документа
+  //       var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+  //       if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+  //           $('.about-statistics__item h3').spincrement({
+  //               thousandSeparator: "",
+  //               duration: 2000
+  //           });
+  //           show = false;
+  //       }
+  //   });
+
+
+  
+  // //--------------------------------------scroll------------------------------
+  //   $('.chat__content').jScrollPane();
+
   
 });
 
@@ -111,3 +117,8 @@ $(function() {
     $('.preloader').delay(1000).fadeOut('slow');
   });
 
+// //--------------------------------------scroll------------------------------
+
+//   $(window).resize(function(event) {
+//     $('.chat__content').jScrollPane();
+//   });
